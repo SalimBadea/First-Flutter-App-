325f41165ae98ae2ff73e41682563d88eb6303de
Salim Badiea <37668158+SalimBadea@users.noreply.github.com>
2022-11-15 20:17:41 +0200

    add web and desktop platforms

---
+++ lib/modules/business/business_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+import '../../models/users.dart';
+
+class BusinessScreen extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Business'),
+    );
+  }
+}
+
+
